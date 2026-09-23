26f208fc06c59a2ae75ec7d38f3f455c7e94df14
BlackBerry8973 <113360494+BlackBerry8973@users.noreply.github.com>
2026-09-23 14:47:59 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+export const TABLE = [
+    {
+        id: 1,
+        status: 'Available'
+    },
+    {
+        id: 2,
+        status: 'Occupied'
+    },
+    {
+        id: 3,
+        status: 'Reserved'
+    },
+    {
+        id: 4,
+        status: 'Cleaning'
+    }
+]
