2a47ede634ac3ed4483f92d643ac030040ac676b
Kusumnipa RATCHANO <kusumnipa.r@ku.th>
2026-09-25 17:48:36 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -2,9 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import "./global.css"
 import Navigation from './navigation';
+import { Provider } from 'react-redux';
+import { store } from './store'
+import { SQLiteProvider } from 'expo-sqlite';
+import { initializeDatabase } from './db/database';
+
 
 export default function App() {
   return (
-    <Navigation />
+    <Provider store={store}>
+      <SQLiteProvider
+        databaseName="db.db" 
+        onInit={initializeDatabase}
+      >
+        <Navigation />
+      </SQLiteProvider>
+    </Provider>
+
   );
 }
--- navigation.js
+++ navigation.js
@@ -1,11 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+import { View, Text } from 'react-native'
+import React from 'react';
 import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen'
-import TableSelectScreen from './screens/TableSelectScreen'
-import OrderScreen from './screens/OrderScreen'
-
-const Stack = createNativeStackNavigator();
+import TableStatusScreen from './screens/TableStatusScreen';
+import CartScreen from './screens/CartScreen';
 
 export default function Navigation() {
     return (
@@ -13,10 +14,11 @@ export default function Navigation() {
             <Stack.Navigator screenOptions={{
                 headerShown: false
             }}>
-                <Stack.Screen name="Order" component={OrderScreen}/>
-                <Stack.Screen name="TableSelect" component={TableSelectScreen}/>
+                {/* <Stack.Screen name="TableStatus" component={TableStatusScreen}/> */}
                 <Stack.Screen name="Home" component={HomeScreen}/>
                 <Stack.Screen name="Restaurant" component={RestaurantScreen}/>
+                <Stack.Screen name="Cart"  options={{presentation: 'modal'}} component={CartScreen}/>
+                
             </Stack.Navigator>
         </NavigationContainer>
     )
